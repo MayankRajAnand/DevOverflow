d161e1d8486afb9fafb61ee42e7ccce02401cbcb
Mayank <mayankrajanand2001@gmail.com>
2023-12-30 16:14:18 +0530

    Implemented theme provider

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-
 import { UserButton } from "@clerk/nextjs";
+import { useTheme } from "@/context/ThemeProvider";
 
 export default function Home() {
+  const { mode } = useTheme();
   return (
-    <div className="h-screen">
+    <div className="flex-center h-screen">
       <UserButton afterSignOutUrl="/" />
     </div>
   );
